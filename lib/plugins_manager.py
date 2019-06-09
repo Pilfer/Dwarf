b8acdb98f7740bccf44201c2f2883eb9c2d34206
PinkiePieStyle <svenw1980@gmail.com>
2019-06-09 08:12:16 +0200

    Add files via upload

--- lib/plugins_manager.py
+++ lib/plugins_manager.py
@@ -1,31 +1,48 @@
 import os
+import inspect
 import importlib.util
-
+from lib.plugin import DwarfPlugin
 
 class PluginsManager:
 
     def __init__(self, app):
         self._app = app
-        self._plugins_path = '/'.join(os.path.realpath(__file__).split('/')[:-2]) + '/plugins'
+        self._plugins_path = os.path.join(os.path.sep.join(os.path.realpath(__file__).split(os.path.sep)[:-2]), 'plugins')
 
         self._plugins = {}
 
     def reload_plugins(self):
-        for plugin in os.listdir(self._plugins_path):
-            spec = importlib.util.spec_from_file_location('', location=self._plugins_path + '/' + plugin)
+        for _, plugin_dirs, _ in os.walk(self._plugins_path):
+            for plugin in [x for x in plugin_dirs if x != '__pycache__']:
+                plugin_folder = [y for y in os.listdir(os.path.join(self._plugins_path, plugin)) if y != '__pycache__']
+                for plugin_file in plugin_folder:
+                    spec = importlib.util.spec_from_file_location('', location=self._plugins_path + os.path.sep + plugin + os.path.sep + plugin_file)
+                    _plugin = importlib.util.module_from_spec(spec)
 
-            # __pycache__
-            if plugin.startswith('__'):
-                continue
+                    if not spec:
+                        continue
 
-            _plugin = importlib.util.module_from_spec(spec)
-            spec.loader.exec_module(_plugin)
-            try:
-                _plugin = _plugin.Plugin()
-                self._plugins[_plugin.get_name()] = _plugin
-                _plugin.on_plugin_loaded()
-            except Exception as e:
-                print('failed to load plugin %s: %s' % (plugin, str(e)))
+                    _plugin = importlib.util.module_from_spec(spec)
+                    spec.loader.exec_module(_plugin)
+
+                    _classes = inspect.getmembers(_plugin, predicate=inspect.isclass)
+                    for _, _class in _classes:
+                        if inspect.isclass(_class) and not inspect.isabstract(_class):
+                            if not _class.__name__.endswith('Plugin'):
+                                continue
+
+                            if _class.__name__ == 'DwarfPlugin':
+                                continue
+
+                            if not isinstance(_class(), DwarfPlugin):
+                                continue
+
+                            try:
+                                _plugin = _class()
+                                self._plugins[_plugin.get_name()] = _plugin
+                                _plugin.on_plugin_loaded()
+                            except Exception as e:
+                                print('failed to load plugin %s: %s' % (plugin, str(e)))
 
     def on_session_started(self):
         for plugin_name in self._plugins.keys():
